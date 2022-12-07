a9f0f1f4d2cb7d293cbe98290273309e3539685d
nororo <wlgus4401@gmail.com>
2022-12-07 20:04:08 +0900

    12/07 수정

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -29,6 +29,27 @@
 		}
 	};
 
+	// Easy on scroll event listener
+	const onscroll = (el, listener) => {
+		el.addEventListener('scroll', listener);
+	};
+
+	// Scrolls to an element with header offset
+	const scrollto = (el) => {
+		let header = select('#header');
+		let offset = header.offsetHeight;
+
+		if (!header.classList.contains('header-scrolled')) {
+			offset -= 20;
+		}
+
+		let elementPos = select(el).offsetTop;
+		window.scrollTo({
+			top: elementPos - offset,
+			behavior: 'smooth',
+		});
+	};
+
 	// Mobile nav toggle
 	on('click', '.mobile-nav-toggle', function (e) {
 		select('#navbar').classList.toggle('navbar-mobile');
@@ -48,6 +69,41 @@
 		},
 		true
 	);
+
+	// Scrool with ofset on links with a class name .scrollto
+	on(
+		'click',
+		'.scrollto',
+		function (e) {
+			if (select(this.hash)) {
+				e.preventDefault();
+
+				let navbar = select('#navbar');
+				if (navbar.classList.contains('navbar-mobile')) {
+					navbar.classList.remove('navbar-mobile');
+					let navbarToggle = select('.mobile-nav-toggle');
+					navbarToggle.classList.toggle('bi-list');
+					navbarToggle.classList.toggle('bi-x');
+				}
+				scrollto(this.hash);
+			}
+		},
+		true
+	);
+
+	// Toggle .header-scrolled class to #header when page is scrolled
+	let selectHeader = select('#header');
+	if (selectHeader) {
+		const headerScrolled = () => {
+			if (window.scrollY > 100) {
+				selectHeader.classList.add('header-scrolled');
+			} else {
+				selectHeader.classList.remove('header-scrolled');
+			}
+		};
+		window.addEventListener('load', headerScrolled);
+		onscroll(document, headerScrolled);
+	}
 
 	// Animation on scroll
 	window.addEventListener('load', () => {
